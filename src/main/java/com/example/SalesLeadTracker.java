9ffb1c335407c56971919f634239e3d8918c7b9a
harshit700 <harshit32670@gmail.com>
2025-06-01 13:31:30 +0530

    commenting not runnable code for master branch. As it is only used for setting up the project

--- src/main/java/com/example/SalesLeadTracker.java
+++ src/main/java/com/example/SalesLeadTracker.java
@@ -1,24 +1,24 @@
 package com.example;
 
-import java.time.LocalDate;
+//import java.time.LocalDate;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class SalesLeadTracker {
     public static void main(String[] args) {
-        var person1 = new Person("Hrithik", null, "Roshan"
-                , LocalDate.of(1994, 3, 4), "hrithik@example.com", null);
-
-        addPersonToLeadTracker(person1);
-
-        var person2 = new Person("ShahRukh", null, "Khan"
-                , LocalDate.of(1965, 11, 2), "sh@example.com", null);
-
-        addPersonToLeadTracker(person2);
+//        var person1 = new Person("Hrithik", null, "Roshan"
+//                , LocalDate.of(1994, 3, 4), "hrithik@example.com", null);
+//
+//        addPersonToLeadTracker(person1);
+//
+//        var person2 = new Person("ShahRukh", null, "Khan"
+//                , LocalDate.of(1965, 11, 2), "sh@example.com", null);
+//
+//        addPersonToLeadTracker(person2);
     }
 
-    private static void addPersonToLeadTracker(Person person) {
-        System.out.println("A new lead was added to the lead tracker: \n" + person);
-    }
+//    private static void addPersonToLeadTracker(Person person) {
+//        System.out.println("A new lead was added to the lead tracker: \n" + person);
+//    }
 
 }
